9f7af1617540b9dfbb654b00fc32bf2e59931445
Hung-I Wang <whygowe@gmail.com>
2019-09-11 11:56:31 +0800

    Just works

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
+#![feature(type_alias_enum_variants)]
+
 extern crate cfg_if;
 extern crate wasm_bindgen;
 
+#[macro_use]
 mod utils;
 
+use std::net::Ipv4Addr;
+use std::cmp::min;
+use std::fmt::Write;
 use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
+
+use utils::{math_log2, set_panic_hook, log};
 
 cfg_if! {
     // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -16,7 +24,72 @@ cfg_if! {
     }
 }
 
+#[derive(Debug)]
+enum Error {
+    RIRStatsMalformed(usize)
+}
+
+impl From<Error> for JsValue {
+    fn from(err: Error) -> JsValue {
+        match err {
+            Error::RIRStatsMalformed(lineno) => JsValue::from(format!("RIR stats is malformed at line {}.", lineno)),
+        }
+    }
+}
+
 #[wasm_bindgen]
 pub fn greet() -> String {
     "Hello, wasm-worker!".to_string()
+}
+
+
+
+#[wasm_bindgen]
+pub fn routes_from_rir_stats(raw_data: &str, country: &str) -> Result<String, JsValue> {
+    set_panic_hook();
+    // https://www.apnic.net/about-apnic/corporate-documents/documents/resource-guidelines/rir-statistics-exchange-format/
+    let mut raw_entries: Vec<(u32, u32)> = Vec::new();
+    for (index, entry) in raw_data.split("\n").enumerate() {
+        if !entry.starts_with("#") && !entry.is_empty() {
+            let fields: Vec<&str> = entry.split("|").collect();
+            if fields.len() >= 7 && fields[1] == country && fields[2] == "ipv4" {
+                let ip: Ipv4Addr = fields[3].parse().map_err(|_e| Error::RIRStatsMalformed(index + 1))?;
+                let count: u32 = fields[4].parse().map_err(|_e| Error::RIRStatsMalformed(index + 1))?;
+                let [a, b, c, d] = ip.octets();
+                let ip = (a as u32) << 24 | (b as u32) << 16 | (c as u32) << 8 | d as u32;
+                raw_entries.push((ip, count));
+            }
+        }
+    }
+    let raw_len = raw_entries.len();
+    raw_entries.sort();
+    let mut merged_entries = Vec::new();
+    let mut last_entry = raw_entries[0];
+    for entry in raw_entries.into_iter().skip(1) {
+        if entry.0 - last_entry.0 == last_entry.1 {
+            last_entry = (last_entry.0, last_entry.1 + entry.1)
+        }
+        else {
+            merged_entries.push(last_entry);
+            last_entry = entry;
+        }
+    }
+    if last_entry != *merged_entries.last().unwrap() /* FIX */ {
+        merged_entries.push(last_entry);
+    }
+    let mut output = String::new();
+    let mut line = 0;
+    for (mut ip, mut count) in merged_entries.into_iter() {
+        while count != 0 {
+            let bits = math_log2(count);
+            let b = min(min(count, 2u32.pow(bits)), 2u32.pow(ip.trailing_zeros()));
+            write!(output, "{}/{}\n", Ipv4Addr::from(ip), 32 - bits).unwrap();
+            line += 1;
+            assert!(count > 0);
+            count -= b;
+            ip += b;
+        }
+    }
+    console_log!("Total: {}/{}", line, raw_len);
+    return Ok(output);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
 
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -14,4 +15,28 @@ cfg_if! {
         #[inline]
         pub fn set_panic_hook() {}
     }
+}
+
+// https://users.rust-lang.org/t/logarithm-of-integers/8506/5
+pub fn math_log2(value: u32) -> u32 {
+    std::mem::size_of::<u32>() as u32 * 8 - value.leading_zeros() - 1
+}
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+}
+
+// Next let's define a macro that's like `println!`, only it works for
+// `console.log`. Note that `println!` doesn't actually work on the wasm target
+// because the standard library currently just eats all output. To get
+// `println!`-like behavior in your app you'll likely want a macro like this.
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
